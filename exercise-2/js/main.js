443d9c7b4a80ef9c34aaac82dbb70f698eef14a9
ABigBigWhale <chakalakafrank@gmail.com>
2016-10-27 09:27:27 -0700

    Doing e2

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -2,39 +2,51 @@
 $(function() {
 
 	// Setup: Initialize Firebase using the configuration of your project
-
+    var config = {
+        apiKey: "AIzaSyCeoK36IJqt7R2HNO8KzMBNHiZeotDCOfc",
+        authDomain: "info343-m12-e2.firebaseapp.com",
+        databaseURL: "https://info343-m12-e2.firebaseio.com",
+        storageBucket: "info343-m12-e2.appspot.com",
+        messagingSenderId: "688551405119"
+    };
+    firebase.initializeApp(config);
 
 	// Set a reference to a "photos" point in your database
-
+    var photosRef = firebase.database().ref('photos');
 
 	// Create a variable to store the firebase storage object
-
+    var storage = firebase.storage();
 
 	// Set listener: when an child is added, render each photo
-
-		// Get the value of the data
-
+    photosRef.on('child_added', function(snapshot){
+        // Get the value of the data
+        var data = snapshot.val();
 
 		// Using jQuery, create a new img element with the URL of your data
-
+        var img = $('img').attr('src', data.url);
 
 		// Append your img to your element with id photos
-
-
-
+        $('#photos').append(img);
+    });
 
 	// Reading Data: Form submission
+    
+    // Get the file
+    var uploadedFile = $('#file-upload')[0].files[0];
 
-		// Get the file
+    // Create a reference on Firebase storage using the filename
+    var uploadRef = storage.ref(uploadedFile);
+    
+    // Put a file in the specified location, then...
+    fileRef.put(uploadedFile).then(function(){
+        
+        
+        
+    });
 
+    
 
-		// Create a reference on Firebase storage using the filename
+        // Get the download URL from the reference, then...
 
-
-		// Put a file in the specified location, then...
-
-			// Get the download URL from the reference, then...
-
-				// Push the URL as a new child into your data structure
-
-		
+            // Push the URL as a new child into your data structure
+})
